4ef836e5976e47a1871c0ad070f2e47699673f51
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-11 16:13:26 +0530

    added graphic user interface to tictactoe and quiz

--- project.py
+++ project.py
@@ -2,104 +2,83 @@ import random
 import tkinter as tk
 import requests
 
-games = ['completelyrics', 'tictactoe', 'quiz']
-
-
-def main():
-    root = tk.Tk()
-    root.title("Random Game")
-    root.geometry("200x100")
-
-    btn = tk.Button(root, text="Play Random Game", command=run_random_game)
-    btn.pack()
-
-    root.mainloop()
+games = ['tictactoe', 'quiz']
 
 
 def run_random_game():
     game = random.choice(games)
-    if game == 'completelyrics':
-        completelyrics()
-    elif game == 'tictactoe':
-        tictactoe()
+    if game == 'tictactoe':
+        play_tictactoe_game()
     elif game == 'quiz':
-        quiz()
+        play_quiz_game()
 
 
-def tictactoe():
+def play_tictactoe_game():
     board = {
         'T1': ' ', 'T2': ' ', 'T3': ' ',
         'M1': ' ', 'M2': ' ', 'M3': ' ',
         'D1': ' ', 'D2': ' ', 'D3': ' '
     }
 
-    player = 1
+    player = 'X'
     total_moves = 0
-    end_check = 0
+    end_check = False
 
-    def check():
-        winning_combinations = [
-            ['T1', 'T2', 'T3'],
-            ['M1', 'M2', 'M3'],
-            ['D1', 'D2', 'D3'],
-            ['T1', 'M2', 'D3'],
-            ['T1', 'M1', 'D1'],
-            ['T2', 'M2', 'D2'],
-            ['T3', 'M3', 'D3'],
-            ['T1', 'M2', 'D3']
+    def check_winner():
+        winning_conditions = [
+            ['T1', 'T2', 'T3'], ['M1', 'M2', 'M3'], ['D1', 'D2', 'D3'],
+            ['T1', 'M1', 'D1'], ['T2', 'M2', 'D2'], ['T3', 'M3', 'D3'],
+            ['T1', 'M2', 'D3'], ['T3', 'M2', 'D1']
         ]
+        for condition in winning_conditions:
+            if board[condition[0]] == board[condition[1]] == board[condition[2]] != ' ':
+                return board[condition[0]]
+        return ''
 
-        for combination in winning_combinations:
-            if board[combination[0]] == board[combination[1]] == board[combination[2]] != ' ':
-                return True
+    def handle_click(key):
+        nonlocal player, total_moves, end_check
+        if not end_check and board[key] == ' ':
+            board[key] = player
+            buttons[key].config(text=player)
+            total_moves += 1
 
-        return False
-
-    print('T1|T2|T3')
-    print('- +- +-')
-    print('M1|M2|M3')
-    print('- +- +-')
-    print('D1|D2|D3')
-    print('***************************')
-
-    while True:
-        print(board['T1'] + '|' + board['T2'] + '|' + board['T3'])
-        print('-+-+-')
-        print(board['M1'] + '|' + board['M2'] + '|' + board['M3'])
-        print('-+-+-')
-        print(board['D1'] + '|' + board['D2'] + '|' + board['D3'])
-        end_check = check()
-
-        if total_moves == 9 or end_check:
-            break
-
-        while True:
-            if player == 1:
-                player_input = input('Player one: ')
+            winner = check_winner()
+            if winner:
+                result_label.config(text=f"Player {winner} wins!")
+                end_check = True
+            elif total_moves == 9:
+                result_label.config(text="It's a tie!")
+                end_check = True
             else:
-                player_input = input('Player two: ')
+                player = 'O' if player == 'X' else 'X'
 
-            if player_input in board and board[player_input] == ' ':
-                board[player_input] = 'X' if player == 1 else 'O'
-                player = 2 if player == 1 else 1
-                total_moves += 1
-                break
-            else:
-                print('Invalid input, please try again')
+    root = tk.Tk()
+    root.title("Tic-Tac-Toe")
+    root.geometry("300x300")
 
-        print('***************************')
-        print()
+    buttons = {}
+    row = 0
+    col = 0
+    for key in board:
+        button = tk.Button(root, text=' ', width=10, height=5, command=lambda key=key: handle_click(key))
+        button.grid(row=row, column=col)
+        buttons[key] = button
 
-    if end_check:
-        print('Player', player, 'won!')
-    else:
-        print('It\'s a draw!')
+        col += 1
+        if col > 2:
+            col = 0
+            row += 1
+
+    result_label = tk.Label(root, text="", pady=10)
+    result_label.grid(row=row, columnspan=3)
+
+    root.mainloop()
 
 
-def quiz():
+def play_quiz_game():
     API_BASE_URL = "https://opentdb.com/api.php"
-    API_AMOUNT = 5
-    API_CATEGORY = 9
+    API_AMOUNT = 5  # Number of questions to retrieve
+    API_CATEGORY = 9  # Category ID for General Knowledge
     API_DIFFICULTY = "medium"
 
     def get_quiz_questions():
@@ -114,67 +93,84 @@ def quiz():
         if response.status_code == 200:
             data = response.json()
             results = data.get("results", [])
+
+            # Add 'correct_answer_index' key to each question
+            for question in results:
+                correct_index = random.randint(0, 3)
+                question['correct_answer_index'] = correct_index
+
             return results
         else:
             print("Failed to retrieve quiz questions. Please try again.")
-            print("Failed to retrieve quiz questions. Please try again.")
 
-    def play_quiz_game():
-        questions = get_quiz_questions()
-        if questions:
-            for question in questions:
-                print(question["question"])
-                options = random.sample(question["incorrect_answers"], 2)
-                options.append(question["correct_answer"])
-                random.shuffle(options)
-
-                for i, option in enumerate(options):
-                    print(f"{i + 1}. {option}")
-
-                answer = input("Your answer (1-3): ")
-                if answer == str(options.index(question["correct_answer"]) + 1):
-                    print("Correct!")
-                else:
-                    print("Incorrect!")
-                print()
-
-    print("Welcome to the Quiz Game!")
-    play_quiz_game()
-
-
-def completelyrics():
-    API_BASE_URL = "https://api.lyrics.ovh/v1"
-
-    def get_song_lyrics(artist, title):
-        endpoint = f"{API_BASE_URL}/{artist}/{title}"
-        response = requests.get(endpoint)
-        if response.status_code == 200:
-            data = response.json()
-            lyrics = data.get("lyrics", "")
-            return lyrics
+    def handle_answer_selection(answer_index, question):
+        if answer_index == question["correct_answer_index"]:
+            result_label.config(text="Correct!", fg="green")
         else:
-            print("Failed to retrieve song lyrics. Please try again.")
+            result_label.config(text="Incorrect!", fg="red")
 
-    def play_complete_lyrics_game():
-        artist = input("Enter the artist name: ")
-        title = input("Enter the song title: ")
+    def display_quiz_question(question):
+        question_label.config(text=question["question"])
+        answers = question["incorrect_answers"]
+        answers.insert(question["correct_answer_index"], question["correct_answer"])
 
-        lyrics = get_song_lyrics(artist, title)
-        if lyrics:
-            print("Complete the lyrics:")
-            print(lyrics)
-            completion = input("Your answer: ")
+        for i, answer in enumerate(answers):
+            button = answer_buttons[i]
+            button.config(text=answer)
+            button.config(command=lambda index=i, q=question: handle_answer_selection(index, q))
 
-            if completion.lower() in lyrics.lower():
-                print("Correct!")
-            else:
-                print("Incorrect!")
+    def next_question():
+        nonlocal current_question_index
+        current_question_index += 1
+
+        if current_question_index < len(quiz_questions):
+            display_quiz_question(quiz_questions[current_question_index])
         else:
-            print("No lyrics found for the specified artist and song.")
+            quiz_frame.destroy()
+            result_label.config(text="Quiz Completed!")
 
-    print("Welcome to the Complete the Lyrics Game!")
-    play_complete_lyrics_game()
+    quiz_questions = get_quiz_questions()
+    if quiz_questions:
+        current_question_index = 0
+
+        root = tk.Tk()
+        root.title("Quiz Game")
+        root.geometry("400x300")
+
+        quiz_frame = tk.Frame(root)
+        quiz_frame.pack(pady=10)
+
+        question_label = tk.Label(quiz_frame, text="", wraplength=380)
+        question_label.pack(pady=10)
+
+        answer_buttons = []
+        for i in range(4):
+            button = tk.Button(quiz_frame, text="", width=40, command=lambda: None)
+            button.pack(pady=5)
+            answer_buttons.append(button)
+
+        result_label = tk.Label(root, text="", pady=10)
+        result_label.pack()
+
+        next_button = tk.Button(root, text="Next", command=next_question)
+        next_button.pack(pady=10)
+
+        display_quiz_question(quiz_questions[current_question_index])
+
+        root.mainloop()
+    else:
+        print("Failed to retrieve quiz questions. Please try again.")
 
 
-if __name__ == "__main__":
-    main()
+def run_game():
+    run_random_game()
+
+
+root = tk.Tk()
+root.title("Random Game Selector")
+root.geometry("200x100")
+
+button = tk.Button(root, text="Run Random Game", command=run_game)
+button.pack(pady=20)
+
+root.mainloop()
