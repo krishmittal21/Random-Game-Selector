008d582e87f8e219f5a0eb16e5dfc99b7c4fecea
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-11 15:47:25 +0530

    fixed the button and improved the code overall

--- project.py
+++ project.py
@@ -4,14 +4,6 @@ import requests
 
 games = ['completelyrics', 'tictactoe', 'quiz']
 
-def run_random_game():
-    game = random.choice(games)
-    if game == 'completelyrics':
-        completelyrics()
-    elif game == 'tictactoe':
-        tictactoe()
-    elif game == 'quiz':
-        quiz()
 
 def main():
     root = tk.Tk()
@@ -22,6 +14,18 @@ def main():
     btn.pack()
 
     root.mainloop()
+
+
+def run_random_game():
+    game = random.choice(games)
+    if game == 'completelyrics':
+        completelyrics()
+    elif game == 'tictactoe':
+        tictactoe()
+    elif game == 'quiz':
+        quiz()
+
+
 def tictactoe():
     board = {
         'T1': ' ', 'T2': ' ', 'T3': ' ',
@@ -32,52 +36,24 @@ def tictactoe():
     player = 1
     total_moves = 0
     end_check = 0
-    high = 0
 
     def check():
-        if board['T1'] == 'X' and board['T2'] == 'X' and board['T3'] == 'X':
-            print('Player one won !')
-            return 1
-        if board['M1'] == 'X' and board['M2'] == 'X' and board['M3'] == 'X':
-            print('Player One Won!!')
-            return 1
-        if board['D1'] == 'X' and board['D2'] == 'X' and board['D3'] == 'X':
-            print('Player One Won!!')
-            return 1
-        if board['T1'] == 'X' and board['M2'] == 'X' and board['D3'] == 'X':
-            print('Player One Won!!')
-            return 1
-        if board['T1'] == 'X' and board['M1'] == 'X' and board['D1'] == 'X':
-            print('Player One Won!!')
-            return 1
-        if board['T2'] == 'X' and board['M2'] == 'X' and board['D2'] == 'X':
-            print('Player One Won!!')
-            return 1
-        if board['T3'] == 'X' and board['M3'] == 'X' and board['D3'] == 'X':
-            print('Player One Won!!')
-            return 1
-        if board['T1'] == 'O' and board['T2'] == 'O' and board['T3'] == 'O':
-            print('Player Two Won!!')
-            return 1
-        if board['M1'] == 'O' and board['M2'] == 'O' and board['M3'] == 'O':
-            print('Player Two Won!!')
-            return 1
-        if board['D1'] == 'O' and board['D2'] == 'O' and board['D3'] == 'O':
-            print('Player Two Won!!')
-            return 1
-        if board['T1'] == 'O' and board['M2'] == 'O' and board['D3'] == 'O':
-            print('Player Two Won!!')
-            return 1
-        if board['T1'] == 'O' and board['M1'] == 'O' and board['D1'] == 'O':
-            print('Player Two Won!!')
-            return 1
-        if board['T2'] == 'O' and board['M2'] == 'O' and board['D2'] == 'O':
-            print('Player Two Won!!')
-            return 1
-        if board['T3'] == 'O' and board['M3'] == 'O' and board['D3'] == 'O':
-            print('Player Two Won!!')
-            return 1
-        return 0
+        winning_combinations = [
+            ['T1', 'T2', 'T3'],
+            ['M1', 'M2', 'M3'],
+            ['D1', 'D2', 'D3'],
+            ['T1', 'M2', 'D3'],
+            ['T1', 'M1', 'D1'],
+            ['T2', 'M2', 'D2'],
+            ['T3', 'M3', 'D3'],
+            ['T1', 'M2', 'D3']
+        ]
+
+        for combination in winning_combinations:
+            if board[combination[0]] == board[combination[1]] == board[combination[2]] != ' ':
+                return True
+
+        return False
 
     print('T1|T2|T3')
     print('- +- +-')
@@ -86,41 +62,44 @@ def tictactoe():
     print('D1|D2|D3')
     print('***************************')
 
-    while 0 < 1:
+    while True:
         print(board['T1'] + '|' + board['T2'] + '|' + board['T3'])
         print('-+-+-')
         print(board['M1'] + '|' + board['M2'] + '|' + board['M3'])
         print('-+-+-')
         print(board['D1'] + '|' + board['D2'] + '|' + board['D3'])
         end_check = check()
-        if total_moves == 9 or end_check == 1:
+
+        if total_moves == 9 or end_check:
             break
+
         while True:
             if player == 1:
-                p1_input = input('player one')
-                if p1_input.upper() in board and board[p1_input.upper()] == ' ':
-                    board[p1_input.upper()] = 'X'
-                    player = 2
-                    break
-                else:
-                    print('Invalid input, please try again')
-                    continue
+                player_input = input('Player one: ')
             else:
-                p2_input = input('player two')
-                if p2_input.upper() in board and board[p2_input.upper()] == ' ':
-                    board[p2_input.upper()] = 'O'
-                    player = 1
-                    break
-                else:
-                    print('Invalid input, please try again')
-                    continue
-        total_moves += 1
+                player_input = input('Player two: ')
+
+            if player_input in board and board[player_input] == ' ':
+                board[player_input] = 'X' if player == 1 else 'O'
+                player = 2 if player == 1 else 1
+                total_moves += 1
+                break
+            else:
+                print('Invalid input, please try again')
+
         print('***************************')
         print()
+
+    if end_check:
+        print('Player', player, 'won!')
+    else:
+        print('It\'s a draw!')
+
+
 def quiz():
     API_BASE_URL = "https://opentdb.com/api.php"
-    API_AMOUNT = 5  # Number of questions to retrieve
-    API_CATEGORY = 9  # Category ID for General Knowledge
+    API_AMOUNT = 5
+    API_CATEGORY = 9
     API_DIFFICULTY = "medium"
 
     def get_quiz_questions():
@@ -137,6 +116,7 @@ def quiz():
             results = data.get("results", [])
             return results
         else:
+            print("Failed to retrieve quiz questions. Please try again.")
             print("Failed to retrieve quiz questions. Please try again.")
 
     def play_quiz_game():
@@ -158,14 +138,13 @@ def quiz():
                     print("Incorrect!")
                 print()
 
-    def main():
-        print("Welcome to the Quiz Game!")
-        play_quiz_game()
+    print("Welcome to the Quiz Game!")
+    play_quiz_game()
 
-    if __name__ == "__main__":
-        main()
+
 def completelyrics():
     API_BASE_URL = "https://api.lyrics.ovh/v1"
+
     def get_song_lyrics(artist, title):
         endpoint = f"{API_BASE_URL}/{artist}/{title}"
         response = requests.get(endpoint)
@@ -176,30 +155,25 @@ def completelyrics():
         else:
             print("Failed to retrieve song lyrics. Please try again.")
 
-        def play_complete_lyrics_game():
-            artist = input("Enter the artist name: ")
-            title = input("Enter the song title: ")
+    def play_complete_lyrics_game():
+        artist = input("Enter the artist name: ")
+        title = input("Enter the song title: ")
 
-            lyrics = get_song_lyrics(artist, title)
-            if lyrics:
-                print("Complete the lyrics:")
-                print(lyrics)
-                completion = input("Your answer: ")
+        lyrics = get_song_lyrics(artist, title)
+        if lyrics:
+            print("Complete the lyrics:")
+            print(lyrics)
+            completion = input("Your answer: ")
 
-                # Compare the completion with the actual lyrics
-                if completion.lower() in lyrics.lower():
-                    print("Correct!")
-                else:
-                    print("Incorrect!")
+            if completion.lower() in lyrics.lower():
+                print("Correct!")
             else:
-                print("No lyrics found for the specified artist and song.")
+                print("Incorrect!")
+        else:
+            print("No lyrics found for the specified artist and song.")
 
-        def main():
-            print("Welcome to the Complete the Lyrics Game!")
-            play_complete_lyrics_game()
-
-        if __name__ == "__main__":
-            main()
+    print("Welcome to the Complete the Lyrics Game!")
+    play_complete_lyrics_game()
 
 
 if __name__ == "__main__":
